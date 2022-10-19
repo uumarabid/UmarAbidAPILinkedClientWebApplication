bac3259c4e199a5f731c9c4b33794a7f2954663e
Umar1311829 <star_umar2008@hotmail.com>
2022-10-19 20:11:39 +0100

    fetch data successfully

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // fetch() method is used 
 // This will return promise 
 // Promise (JS) will either fullfilled or rejected .
+// https://data.police.uk/docs/method/crime-categories/
+
 
 fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
     console.log(data);
@@ -8,6 +10,26 @@ fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
     return data.json();
     // pass the data to check in the console, assuming the promise has fullfilled
 }).then((getData)=> {
-    console.log(getData[1].name);
-    document.getElementById("dispaly").innerText = getData[1].name;
-});
+    // test the data to display on the browser
+    // console.log(getData[1].name);
+    // document.getElementById("dispaly").innerText = getData[1].name;
+
+    let data = "";
+    //using the array 
+    getData.map((values)=>{
+        // template literals are used
+       data += `<div class="row">
+       <div class="col-md-2 shadow p-4 mb-3 bg-body rounded mx-1">
+         <h2>${values.name}</h2>
+         <a href=${values.url}>${values.url}</a>
+       </div>
+       </div>` 
+    });
+
+    // display on the browser
+    document.getElementById("dispaly").innerHTML = data
+
+    // error hander
+}).catch ((error)=>{
+    console.log(error);
+})
