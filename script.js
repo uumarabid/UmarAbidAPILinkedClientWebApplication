4304e7dcecdc964e1c492d2839435d917e682eb5
Umar1311829 <star_umar2008@hotmail.com>
2022-10-19 19:03:06 +0100

    test the data to display on the browser

--- script.js
+++ script.js
@@ -8,5 +8,6 @@ fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
     return data.json();
     // pass the data to check in the console, assuming the promise has fullfilled
 }).then((getData)=> {
-    console.log(getData);
-})
+    console.log(getData[1].name);
+    document.getElementById("dispaly").innerText = getData[1].name;
+});
