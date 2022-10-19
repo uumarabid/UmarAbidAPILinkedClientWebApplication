010cea41db4e5fa81814cefca9d218619ab3ac60
Umar1311829 <star_umar2008@hotmail.com>
2022-10-19 15:08:41 +0100

    receive data in json, convert data into JS object and test in console

--- script.js
+++ script.js
@@ -4,4 +4,9 @@
 
 fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
     console.log(data);
+    // convert the data into  JS object 
+    return data.json();
+    // pass the data to check in the console, assuming the promise has fullfilled
+}).then((getData)=> {
+    console.log(getData);
 })
