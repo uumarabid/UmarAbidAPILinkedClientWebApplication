a5a83353727e55e6aed7bbba2175a32c58f957e8
Umar1311829 <star_umar2008@hotmail.com>
2022-10-20 12:45:23 +0100

    Add search feature to filter the data

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Promise (JS) will either fullfilled or rejected .
 // https://data.police.uk/docs/method/crime-categories/
 
-
+let globalData = null;
 fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
     console.log(data);
     // convert the data into  JS object 
@@ -13,7 +13,7 @@ fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
     // test the data to display on the browser
     // console.log(getData[1].name);
     // document.getElementById("dispaly").innerText = getData[1].name;
-
+    globalData = getData;
     let data = "";
     //using the array 
     getData.map((values)=>{
@@ -21,7 +21,7 @@ fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
        data += `
        <div class="col-md-2 shadow p-4 mb-3 bg-body rounded mx-1">
          <h2>${values.name}</h2>
-         <a href=${values.url}>${values.url}</a>
+         <a href="${values.url}">${values.url}</a>
        </div>
        ` 
     });
@@ -33,3 +33,23 @@ fetch("https://data.police.uk/api/crime-categories?date=2011-08").then((data)=>{
 }).catch ((error)=>{
     console.log(error);
 })
+
+
+function search(){
+    const value  = document.getElementById("searchData").value;
+    const getData = globalData.filter(x => x.name.toLowerCase().includes(value.toLowerCase()));
+    let data = "";
+    getData.map((values)=>{
+        // template literals are used
+       data += `
+       <div class="col-md-2 shadow p-4 mb-3 bg-body rounded mx-1">
+         <h2>${values.name}</h2>
+         <a href="${values.url}">${values.url}</a>
+       </div>
+       ` 
+    });
+
+    // display on the browser
+    document.getElementById("dispaly").innerHTML = data
+
+}
